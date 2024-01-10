ead1bd6b2bffcdecd314d0955dc4ac122c750f26
Ayberk Kaya <kayaayberk98@gmail.com>
2024-01-10 20:01:03 +0100

    Update config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["github.com", "lh3.googleusercontent.com"]
-    }
-}
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ["github.com", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
