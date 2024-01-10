3c8f0c236a991a47b7a9b874410cad44ed75f64d
Ayberk Kaya <kayaayberk98@gmail.com>
2024-01-10 20:24:41 +0100

    Hide API key

--- firebase.ts
+++ firebase.ts
@@ -3,21 +3,21 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "real-time-translation-chat-app.firebaseapp.com",
-    projectId: "real-time-translation-chat-app",
-    storageBucket: "real-time-translation-chat-app.appspot.com",
-    messagingSenderId: "350916461847",
-    appId: "1:350916461847:web:cab925fb743451aac0a352",
-    measurementId: "G-XKN3GTW5ZL"
-  };
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
+  measurementId: process.env.NEXT_PUBLIC_measurementId,
+};
 
-  const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
-  const functions = getFunctions(app);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const functions = getFunctions(app);
 
-  export { app, auth, db, functions}
+export { app, auth, db, functions };
