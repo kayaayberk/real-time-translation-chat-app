0b41c89f848eb772431c35bff35b2ac6a28b65aa
Ayberk Kaya <kayaayberk98@gmail.com>
2024-01-12 01:10:41 +0100

    Create chat pages with chatIds

---
+++ app/(user)/chat/[chatId]/page.tsx
@@ -0,0 +1,20 @@
+
+
+function ChatPage() {
+  return (
+    <>
+        {/* Admin Controls */}
+        
+
+        {/* Chat Members Badge */}
+
+
+        {/* Chat Messages */}
+
+
+        {/* Chat Input */}
+    </>
+  )
+}
+
+export default ChatPage
---
+++ app/(user)/chat/page.tsx
@@ -0,0 +1,14 @@
+import ChatList from "@/components/ChatList";
+
+function ChatsPage() {
+  return (
+    <div>
+      {/* Char Permission chat */}
+
+      {/* ChatList */}
+      <ChatList />
+    </div>
+  );
+}
+
+export default ChatsPage;
