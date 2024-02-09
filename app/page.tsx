a59a0987f0957b52452b2ce8c6ba16f9319b386f
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-09 17:19:47 +0100

    Add demo

--- app/page.tsx
+++ app/page.tsx
@@ -41,14 +41,14 @@ export default async function Home() {
           </div>
           <div className="mt-16 flow-root sm:mt-24" >
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4" >
-              {/* <Image 
+              <Image 
               unoptimized
               src={DemoGif}
               alt="App use gif"
               width={2432}
               height={1442}
               className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
-              /> */}
+              />
             </div>
           </div>
         </div>
